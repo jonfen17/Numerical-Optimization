963c7407cbf5cd2d552b7e9a296111a15e359733
dioney <dio@ney.com>
2015-04-21 12:40:08 -0500

    Functional code for L-BFGS Algorithm

--- L-BFGS/DoubleLoop.m
+++ L-BFGS/DoubleLoop.m
@@ -8,40 +8,40 @@
 % respectivamente. 
 %
 % Input:
-% g - Gradiente en la k-ésima iteración
-% H - Aproximación inicial de la hessiana
-% S - Matriz con columnas dadas por  S(:,i) = X_i+1 - X_i   i = k-m ... k-1
-% Y - Matriz con columnas dadas por  Y(:,i) = g_i+1 - g_i   i = k-m ... k-1 
+% g   - Gradiente en la k-ésima iteración
+% H   - Aproximación inicial de la hessiana
+% S   - Matriz c/ columnas  S(:,i) = X_i+1 - X_i   i = k-m ... k-1
+% Y   - Matriz c/ columnas  Y(:,i) = g_i+1 - g_i   i = k-m ... k-1
+% rho - Vector con entradas 1 / (y' * s); de las m últimas iteraciones
+% k   - Iteración en BFGS
+% m   - Columnas matriz S, Y
 % Output:
 % r - El producto de H*g
 %--------------------------------------------------------------------------
 
-function r = DoubleLoop( g, H, S, Y)
-
-    [ n, m] = size( S );
-    cero = zeros( m, 1);  
-    rho	    = cero;  	alpha   = cero; 	beta    = cero;
-
-    % Calculamos nuestras rho en un vector de k*1
-    for i = 1:m
-       rho(i,1) = 1/(Y(:,i)'*S(:,i)); 
-    end 
+function r = DoubleLoop( g, H, S, Y, rho, k, m)
+    
+    m       = min( k, m);
+    cero    = zeros( 1, m);  
+    alpha   = cero;
+    idx     = backIndex( k, m); % Apuntador hacia atrás
 
     % Realizamos el primer loop
-    for i = m:-1:1
-
-        alpha(i) = rho(i)* S(:,i)'*g;
-        g        = g - alpha(i)* Y(:,i);
+    for i = 1:m
+        j        = idx(i);
+        alpha(j) = rho(j)* S(:,j)'*g;
+        g        = g - alpha(j)* Y(:,j);
 
     end
 
-    r = H*g;
+    r   = H*g;
+    idx = forwIndex( k, m); % Apuntador hacia adelante
     
     % Realizamos el segundo loop
     for i = 1:m
-
-        beta(i) = rho(i)* Y(:,i)'*r;
-        r       = r + S(:,i)*(alpha(i)- beta(i));
+        j    = idx(i);
+        beta = rho(j)* Y(:,j)'*r;
+        r    = r + S(:,j)*(alpha(j)- beta);
         
     end
 
---
+++ L-BFGS/L_BFGS.m
@@ -0,0 +1,96 @@
+%========================================================================
+% Este programa lleva a cabo el método
+% L-BFGS
+% para obtener la solución al sistema de ecuaciones 
+% A*x = b
+% Curso Análisis Aplicado
+% Abril 2015
+% Carlos Dioney Blanco González 
+%========================================================================
+
+function sol = L_BFGS( fun, x0, maxiter, m )
+
+%========================================================================
+% INPUT:
+%  fun      - La función a minimizar
+%  x        - El punto inicial
+% maxiter   - Máximo de iteraciones
+%  m        - Tamaño LBFGS
+%
+% OUTPUT:
+%  sol     - Solución del problema
+%  i       - Número total de iteraciones
+%========================================================================
+tol = 1e-8;               % tolerancia
+i   = 0;                  % iniciamos nuestro contador
+f0  = feval(fun, x0);     % función y gradiente en el punto inicial
+g0  = diffgrad( x0, fun);
+n   = length(x0);       
+S   = zeros( n, m);       % matrices S, Y y Rho para calcular H*g
+Y   = zeros( n, m);
+Rho = zeros( m, 1);
+fprintf(1,'   k        f            ||g||     alfa     nfg     curv  \n');
+fprintf(1,'----------------------------------------------------------\n');
+%========================================================================
+g = g0;      x = x0;        p = -g;
+
+[al, xn, f, gn, ~, nf]  = ...                       % BL con CFW
+    linesch_sw( x, f0, g, p, fun, 0.0001, 0.9, 2); 
+norma = norm(gn);
+s     = xn - x;              % Calculamos S, Y y curvatura
+y     = gn - g;
+curv  = s' * y;
+% Imprimimos y Actualzamos
+fprintf(' %3i  %14.8e   %8.2e   %5.3f    %3i       %5.3f   \n', ...
+                            i, f,  norma, al, nf, curv );
+x = xn;
+g = gn;
+i = 1;
+
+while (norma > tol) && (i < maxiter) && (curv > 0)
+        
+    h    = s'*y/ (y'*y);
+    H    = h * ones( n, n);
+    
+    if   i > m
+        
+        % Actualizamos S, Y que entran a L-BFGS 
+        % Notar que sólo cambiamos una columna ( idx) en cada iteración
+        idx = mod(i,m);
+        
+        if idx == 0
+            idx = m;
+        end
+        
+        Rho(idx) = 1/curv;
+        S(:,idx) = s;
+        Y(:,idx) = y;
+        p        = -DoubleLoop( g, H, S, Y, Rho, i, m);
+        
+    elseif i <= m
+        
+        % Actualizamos S,Y que entran a L-BFGS
+        % En estos pasos se hace el BFGS normal
+        Rho(i)  = 1/curv;
+        S(:, i) = s;
+        Y(:, i) = y;
+        p       = -DoubleLoop( g, H, S(:,1:i), Y(:,1:i), Rho, i, m);
+        
+    end
+    
+    [al, xn, f, gn, ~, nf]  = ...                     % BL con CFW
+        linesch_sw( x, f, g, p, fun, 0.0001, 0.9, 2);
+    % Actualizamos 
+    s     = xn - x;           % S y G para BFGS
+    y     = gn - g;
+    curv  = s' * y;    
+    norma = norm(gn);
+    x     = xn;
+    g     = gn;
+  
+      fprintf(' %3i  %14.8e   %8.2e   %5.3f    %3i       %5.3f   \n', ...
+                            i, f,  norma, al, nf, curv );
+    i = i + 1;
+end
+
+sol = x;  % Regresamos la solución
